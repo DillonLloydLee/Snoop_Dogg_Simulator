18a678fb383aade727de429fc6742cd4f88b3ecd
Nate Young & Dillon Lloyd Lee <student@epicodus.com>
2015-08-10 15:50:22 -0700

    translate esses test complete.

--- src/SnoopTranslator.php
+++ src/SnoopTranslator.php
@@ -3,7 +3,20 @@
     {
         function translate($normal_phrase)
         {
+            $normal_array = str_split($normal_phrase);
+            $snoop_phrase = array();
 
+            foreach($normal_array as $letter) {
+                if ($letter == "s") {
+                    $letter = "z";
+                    array_push($snoop_phrase, $letter);
+                }
+                else {
+                    array_push($snoop_phrase, $letter);
+                }
+            }
+
+            return implode("", $snoop_phrase);
         }
     }
 
